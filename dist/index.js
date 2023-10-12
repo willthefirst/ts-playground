3187186a1e29d3f655510e51e4abdfb89e3d0609
Will Moritz <willthefirst@gmail.com>
2023-10-12 12:22:33 -0700

    Backtracking

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,27 @@
 "use strict";
-console.log('Hi');
+function permute(nums) {
+    const result = [];
+    const remainingChoices = nums.map((_) => true);
+    backtrack(result, nums, [], remainingChoices);
+    return result;
+}
+function backtrack(result, nums, permutation, remainingChoices) {
+    // Goal reached
+    if (permutation.length === nums.length) {
+        result.push(permutation);
+        return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (remainingChoices[i]) {
+            remainingChoices[i] = false;
+            permutation.push(nums[i]);
+            backtrack(result, nums, [...permutation], remainingChoices);
+            remainingChoices[i] = true;
+            permutation.pop();
+        }
+    }
+}
+const source = [1, 2, 3];
+const permutations = permute(source);
+console.log('Permutations:', permutations);
 //# sourceMappingURL=index.js.map
